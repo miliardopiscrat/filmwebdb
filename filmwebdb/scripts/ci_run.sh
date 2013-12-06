b1157e75a4d48f6965cd582351f3007d14c4e895
miliardopiscrat <miliardopiscrat@gmail.com>
2013-12-06 01:35:49 +0100

    toolchain and scripts

---
+++ filmwebdb/scripts/ci_run.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+
+
+export PATH=`pwd`/../compiler/msdk-4.3.6-mips-EL-2.6.34-0.9.30.3_hwfpu/bin:$PATH
+
+ls $(pwd)/../compiler
+
+cd filmwebdb
+
+## no tests yet
+## make TARGET=linux testlib -j4
+
+##./binary/test
+
+make TARGET=linux clean
+make TARGET=mipsel all -j4
+make TARGET=mipsel tarball
